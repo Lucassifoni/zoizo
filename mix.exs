b0d789bec51fe7935c8a66460d6a317e32e7dcfc
Chantepierre <lucas@lucassifoni.info>
2023-09-13 19:35:52 +0200

    Removes unneeded targets

--- mix.exs
+++ mix.exs
@@ -5,22 +5,16 @@ defmodule NervesLivebook.MixProject do
   @version "0.10.1"
   @source_url "https://github.com/nerves-livebook/nerves_livebook"
 
-  @rpi_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4]
-  @all_targets @rpi_targets ++
-                 [:bbb, :osd32mp1, :x86_64, :npi_imx6ull, :grisp2, :mangopi_mq_pro, :srhub]
+  @all_targets [:mangopi_mq_pro]
 
   # Libraries that use MMAL on the Raspberry Pi won't work with the Raspberry
   # Pi 4. The Raspberry Pi 4 uses DRM and libcamera.
-  @rpi_mmal_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a]
 
   # See the BlueHeron repository for the boards that it supports.
-  @ble_targets [:rpi0, :rpi3, :rpi3a]
 
   # Targets supporting cellular modems
-  @cellular_targets [:srhub]
 
   # TFLite isn't building on the RPi and RPi0 (armv6), so just don't include it there.
-  @tflite_targets @all_targets -- [:rpi, :rpi0]
 
   # Instruct the compiler to create deterministic builds to minimize
   # differences between firmware versions. This helps delta firmware update
@@ -89,8 +83,6 @@ defmodule NervesLivebook.MixProject do
       {:vintage_net, "~> 0.13"},
 
       # Pull in commonly used libraries as a convenience to users.
-      {:blue_heron, "~> 0.3", targets: @ble_targets},
-      {:blue_heron_transport_uart, "~> 0.1.2", targets: @ble_targets},
       {:bmp280, "~> 0.2", targets: @all_targets},
       {:circuits_gpio, "~> 1.0"},
       {:circuits_i2c, "~> 1.0"},
@@ -108,34 +100,18 @@ defmodule NervesLivebook.MixProject do
       {:nerves_time_zones, "~> 0.3.0", targets: @all_targets},
       {:nx, "~> 0.5.0"},
       {:phoenix_pubsub, "~> 2.0"},
-      {:picam, "~> 0.4.0", targets: @rpi_mmal_targets},
-      {:pigpiox, "~> 0.1", targets: @rpi_targets},
       {:pinout, "~> 0.1"},
       {:progress_bar, "~> 3.0"},
       {:ramoops_logger, "~> 0.1", targets: @all_targets},
       {:recon, "~> 2.5"},
       {:req, "~> 0.3.0"},
-      {:scroll_hat, "~> 0.1", targets: @rpi_targets},
       {:stb_image, "~> 0.6.0"},
-      {:tflite_elixir, "~> 0.3.4", targets: @tflite_targets},
       {:vega_lite, "~> 0.1"},
-      {:vintage_net_mobile, "~> 0.11", targets: @cellular_targets},
-      {:vintage_net_qmi, "~> 0.3", targets: @cellular_targets},
 
-      # Nerves system dependencies
-      {:nerves_system_rpi, "~> 1.22", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.22", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.22", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.22", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.22", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.22", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.14", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.14", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.22", runtime: false, targets: :x86_64},
-      {:nerves_system_npi_imx6ull, "~> 0.11", runtime: false, targets: :npi_imx6ull},
-      {:nerves_system_grisp2, "~> 0.7", runtime: false, targets: :grisp2},
+
+
       {:nerves_system_mangopi_mq_pro, "~> 0.5", runtime: false, targets: :mangopi_mq_pro},
-      {:nerves_system_srhub, "~> 0.26", runtime: false, targets: :srhub},
+
 
       # Compile-time only
       {:credo, "~> 1.6", only: :dev, runtime: false},
